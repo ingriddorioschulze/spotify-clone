c13471a3e767eebd8db69a911550db9de41fc943
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2020-09-07 14:50:55 +0200

    Finish Footer implementation

--- src/Footer.js
+++ src/Footer.js
@@ -23,35 +23,77 @@ const FooterWrapper = styled.div`
 `
 const FooterLeft = styled.div`
   flex: 0.3;
+  display: flex;
+  align-items: center;
+  color: white;
+  width: 300px;
+
+  .album-cover {
+    height: 60px;
+    width: 60px;
+    margin-right: 20px;
+    object-fit: contain;
+    white-space: nowrap;
+  }
+
+  .footer-song-info > h4 {
+    margin-bottom: 5px;
+  }
+
+  .footer-song-info > p {
+    font-size: 12px;
+  }
 `
 const FooterCenter = styled.div`
   flex: 0.4;
   margin: 0 100px;
-  color: white;
   display: flex;
   align-items: center;
   justify-content: space-between;
   max-width: 300px;
+
+  .footer-green {
+    color: #1ed15e;
+  }
+
+  .footer-icon {
+    color: white;
+  }
+
+  .footer-green:hover,
+  .footer-icon:hover {
+    transition: transform 0.2s ease-in-out;
+    transform: scale(1.2);
+  }
 `
 const FooterRight = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   flex: 0.3;
   color: white;
 
-  .continuous-slider {
-    background-color: white;
+  * .MuiSlider-root {
+    color: #1ed15e;
   }
 `
 
 function Footer() {
   return (
     <FooterWrapper>
-      <FooterLeft>LALALALALALLALA</FooterLeft>
+      <FooterLeft>
+        <img src="" alt="album cover" />
+        <div className="footer-song-info">
+          <h4>Eminência Parda</h4>
+          <p>Emicida</p>
+        </div>
+      </FooterLeft>
 
       <FooterCenter>
         <ShuffleIcon className="footer-green"></ShuffleIcon>
         <SkipPreviousIcon className="footer-icon"></SkipPreviousIcon>
         <PlayCircleOutlineIcon
-          className="footer-green"
+          className="footer-icon"
           fontSize="large"
         ></PlayCircleOutlineIcon>
         <SkipNextIcon className="footer-icon"></SkipNextIcon>
